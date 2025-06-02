a5472c649c087d5772a385e48809469d635daf5b
candreanivera <candreanivera@gmail.com>
2025-06-02 14:26:37 +1200

    Updating App.js file

--- src/App.js
+++ src/App.js
@@ -1,89 +1,96 @@
-import React, { useEffect, useState } from 'react';
-import ExpenseList from './components/ExpenseList';
-import AddExpenseForm from './components/AddExpenseForm';
-import Saludo from './components/Saludo';
-import './Css/App.css';
-
+import React, { useEffect, useState } from "react";
+import ExpenseList from "./components/ExpenseList";
+import AddExpenseForm from "./components/AddExpenseForm";
+import Saludo from "./components/Saludo";
+import "./Css/App.css";
 
 function App() {
   const [expenses, setExpenses] = useState([]);
 
   // To obtain the current month
-  const currentMonth = new Date().toLocaleString('en-US', { month: 'long' });
+  const currentMonth = new Date().toLocaleString("en-US", { month: "long" });
 
   //Hook used to call the API
   //res: Server's answer. res.json() transforms the answer in a javascript object
   //then result is passed to setExpenses, so expenses can be updated
   useEffect(() => {
-    fetch('http://localhost:5186/api/expenses')
-      .then(res => res.json())
-      .then(data => setExpenses(data));
-      //expenses value will be replaced by the value obtained from the server
+    fetch("https://expensetrackerbackend-1-r4ic.onrender.com/")
+      .then((res) => res.json())
+      .then((data) => setExpenses(data));
+    //expenses value will be replaced by the value obtained from the server
   }, []);
 
-
-//ADD EXPENSES
-  const handleAddExpense = newExpense => {
-    
+  //ADD EXPENSES
+  const handleAddExpense = (newExpense) => {
     setExpenses([...expenses, newExpense]);
   };
 
-
-//DELETE EXPENSE
+  //DELETE EXPENSE
   const handleDeleteExpense = async (id) => {
     const res = await fetch(`http://localhost:5186/api/expenses/${id}`, {
-      method: 'DELETE'
+      method: "DELETE",
     });
     if (res.ok) {
       //Removes the eliminated expense from the local list
-      setExpenses(expenses.filter(exp => exp.id !== id));
+      setExpenses(expenses.filter((exp) => exp.id !== id));
     } else {
-      console.error('Error at deleting the expense');
+      console.error("Error at deleting the expense");
     }
   };
 
-
-//EDIT EXPENSE
+  //EDIT EXPENSE
   const handleEditExpense = async (id, updatedExpense) => {
     const res = await fetch(`http://localhost:5186/api/expenses/${id}`, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(updatedExpense),
     });
 
     if (res.ok) {
       const updated = await res.json();
-      setExpenses(expenses =>
-        expenses.map(exp => exp.id === id ? updated : exp));
+      setExpenses((expenses) =>
+        expenses.map((exp) => (exp.id === id ? updated : exp))
+      );
     } else {
-      console.error('Error at editing the expense');
+      console.error("Error at editing the expense");
     }
   };
 
   //To obtain the total expense amount
-  const totalAmount = expenses.reduce((sum, exp) => sum + parseFloat(exp.amount), 0);
+  const totalAmount = expenses.reduce(
+    (sum, exp) => sum + parseFloat(exp.amount),
+    0
+  );
 
   return (
-    <div className = "App">
-      <div className = "main-container">
+    <div className="App">
+      <div className="main-container">
         <header className="logo-contenedor">
           <div>
-            <h1 className='title'>Expense Tracker</h1>
+            <h1 className="title">Expense Tracker</h1>
           </div>
-          <div className='messages'>
-              <Saludo name="Cristina" month={currentMonth} />
-              <p>Total spent this month: <b>${totalAmount.toFixed(2)}</b></p>
+          <div className="messages">
+            <Saludo name="Cristina" month={currentMonth} />
+            <p>
+              Total spent this month: <b>${totalAmount.toFixed(2)}</b>
+            </p>
           </div>
         </header>
         <hr />
         <div>
           <AddExpenseForm onAdd={handleAddExpense} />
         </div>
-        <hr className="line"/>
-        <div style={{ marginBottom: '20px', textAlign: 'center', padding: '10px'}}>
-        <ExpenseList expenses={expenses} onDelete={handleDeleteExpense} onEdit={handleEditExpense} />
+        <hr className="line" />
+        <div
+          style={{ marginBottom: "20px", textAlign: "center", padding: "10px" }}
+        >
+          <ExpenseList
+            expenses={expenses}
+            onDelete={handleDeleteExpense}
+            onEdit={handleEditExpense}
+          />
         </div>
-        <br/>
+        <br />
       </div>
     </div>
   );
